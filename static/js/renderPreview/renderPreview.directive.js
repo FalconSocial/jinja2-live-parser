65145204b58026fba1d1277481e0e1165e371fb7
Lars Krieger <lars.krieger@gmail.com>
2015-10-07 12:25:58 +0200

    Dirty hack for the render to clear the iframe and paste in new values

--- static/js/renderPreview/renderPreview.directive.js
+++ static/js/renderPreview/renderPreview.directive.js
@@ -10,6 +10,13 @@ angular.module('Falcon-email')
 		 		var $frame = element.find('iframe')[0];
 
 		 		scope.$watch('html', function( val ) {
+		 			//Hack to override previews document data.
+		 			//Should be refactored.
+		 			
+		 			$frame.contentWindow.document.open();
+		 			$frame.contentWindow.document.write('');
+		 			$frame.contentWindow.document.close();
+
 		 			if(val) {
 		 				$frame.contentDocument.write(val);
 		 			}
